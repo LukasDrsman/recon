004df3b029d7a1fa10ad3e2fabe6f3aadddeffbe
LukasDrsman <lukaskodr@gmail.com>
2022-04-23 10:06:52 +0200

    Code cleanup

--- src/recon/camera.hpp
+++ src/recon/camera.hpp
@@ -10,7 +10,7 @@ namespace re
 	{
 		public:
 			// camera parameters
-			double O[3], P[7];
+			double O[3], C[2], f[3], p[2];
 
 			Camera(char *fname)
 			{
@@ -20,13 +20,13 @@ namespace re
 				c::paramload(fp, _P, 5);
 
 				// projection parameters
-				P[0] = _P[2] / _P[0];	// W conversion ratio
-				P[1] = _P[3] / _P[1];	// H conversion ratio
-				P[2] = _P[4] / P[0];	// x-dir focal length (fx)
-				P[3] = _P[4] / P[1];	// y-dir focal length (fy)
-				P[4] = 0.5 * _P[0];		// x offset (px)
-				P[5] = 0.5 * _P[1];		// y offset (py)
-				P[6] = _P[4];			// real focal length (f)
+				C[0] = _P[2] / _P[0];	// W conversion ratio
+				C[1] = _P[3] / _P[1];	// H conversion ratio
+				f[0] = _P[4] / C[0];	// x-dir focal length (fx)
+				f[1] = _P[4] / C[1];	// y-dir focal length (fy)
+				f[2] = _P[4];			// real focal length (f)				
+				p[0] = 0.5 * _P[0];		// x offset (px)
+				p[1] = 0.5 * _P[1];		// y offset (py)
 			}
 
 			// project R^3 vector into R^2 camera image (pixel)
@@ -34,15 +34,15 @@ namespace re
 			{
 				double d = 1 / (v[2] - O[2]);
 				for (int i = 0; i < 2; i++)
-					dest[i] = d * P[2 + i] * (v[i] - O[i]) + P[4 + i];
+					dest[i] = d * f[i] * (v[i] - O[i]) + p[i];
 			}
 
 			// construct an R^3 vector to the camera screen from a pixel
 			void pixelVector(double *dest, double *v)
 			{
 				for (int i = 0; i < 2; i++)
-					dest[i] = P[i] * (v[i] - P[4 + i]) + O[i];
-				dest[2] = P[6] + O[2];
+					dest[i] = C[i] * (v[i] - p[i]) + O[i];
+				dest[2] = f[2] + O[2];
 			}
 	};
 }
--- src/recon/stereo.hpp
+++ src/recon/stereo.hpp
@@ -15,23 +15,23 @@ namespace re
 			:   l(_l), r(_r)
 			{
 				// precompute constant reconstruction parameters
-				RP[0] = l.P[2] * l.O[0];	// l{ fx * cx }
-				RP[1] = r.P[2] * r.O[0];	// r{ fx * cx }
+				RP[0] = l.f[0] * l.O[0];	// l{ fx * cx }
+				RP[1] = r.f[0] * r.O[0];	// r{ fx * cx }
 				RP[2] = l.O[2] - r.O[2];	// l{ cz } - r{ cz }
-				RP[3] = 1 / l.P[3];			// l{ 1 / fy }
+				RP[3] = 1 / l.f[1];			// l{ 1 / fy }
 			}
 
 			void triangulate(double *dest, double *u, double *v)
 			{
 				// precompute frequent values
 				double pu, pv, R;
-				pu = l.P[4] - u[0];
-				pv = 1 / (r.P[4] - v[0]);		// pv only used in inverse form
+				pu = l.p[0] - u[0];
+				pv = 1 / (r.p[0] - v[0]);		// pv only used in inverse form
 				R = pu * pv;
 
-				dest[0] = (RP[2] * pu - RP[1] * R + RP[0]) / (l.P[2] - r.P[2] * R);
-				dest[2] = r.O[2] + r.P[2] * (r.O[0] - dest[0]) * pv;
-				dest[1] = (u[1] - l.P[5]) * (dest[2] - l.O[2]) * RP[3] + l.O[1];
+				dest[0] = (RP[2] * pu - RP[1] * R + RP[0]) / (l.f[0] - r.f[0] * R);
+				dest[2] = r.O[2] + r.f[0] * (r.O[0] - dest[0]) * pv;
+				dest[1] = (u[1] - l.p[1]) * (dest[2] - l.O[2]) * RP[3] + l.O[1];
 			}
 	};
 }
